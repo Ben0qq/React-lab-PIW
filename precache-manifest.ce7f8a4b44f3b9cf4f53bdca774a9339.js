6b22757a952bfd6cddd98d2574076d1ee6f88138
Ben0qq <48827870+Ben0qq@users.noreply.github.com>
2020-06-06 18:02:04 +0200

    Updates

---
+++ precache-manifest.ce7f8a4b44f3b9cf4f53bdca774a9339.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27488ba59825d8121f84709c6e45495c",
+    "url": "/React-lab-PIW/index.html"
+  },
+  {
+    "revision": "000a76c3883ac5f7acca",
+    "url": "/React-lab-PIW/static/css/main.a6911744.chunk.css"
+  },
+  {
+    "revision": "e86016ea3fef76984875",
+    "url": "/React-lab-PIW/static/js/2.784806dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-lab-PIW/static/js/2.784806dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "000a76c3883ac5f7acca",
+    "url": "/React-lab-PIW/static/js/main.fa3d845a.chunk.js"
+  },
+  {
+    "revision": "35887f2825a41ffb9f22",
+    "url": "/React-lab-PIW/static/js/runtime-main.a663a84b.js"
+  },
+  {
+    "revision": "f953e41c8aad4e5314c7ecc265fba3de",
+    "url": "/React-lab-PIW/static/media/pwr.f953e41c.png"
+  },
+  {
+    "revision": "a8a627cdae666ee67f78afda994932cb",
+    "url": "/React-lab-PIW/static/media/suprise.a8a627cd.png"
+  }
+]);
